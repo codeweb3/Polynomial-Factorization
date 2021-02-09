ed8ba47be748479fa702f36180d9b54ebd1d24b3
codeweb3 <47444525+codeweb3@users.noreply.github.com>
2021-02-09 14:02:38 +0900

    Add files via upload

---
+++ PF.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+poly = [ ]
+ansrange = 100
+charlist = [ ]
+
+def evaluate(x):
+	a = { }
+	a
+
+inputstr = input() + ';'
+prev = 0;
+
+inlen = len(inputstr)
+for i in inputstr:
+	if ord('a') <= ord(i) <= ord('z'):
+		charlist.append(i)
+charlist = list(dict.fromkeys(charlist))
+if(inputstr[0] != '-'):
+	inputstr = '+' + inputstr
+
+i = 0
+ncounting = False
+while i < inlen:
+	if(inputstr[i] == '-'):
+		poly.append('|')
+		poly.append('-1')
+	elif inputstr[i] == '+' or inputstr[i] == ';': poly.append('|')
+	else:
+		if ord('0') <= ord(inputstr[i]) <= ord('9'):
+			if ncounting == False:
+				ncounting = True
+				poly.append(inputstr[i])
+			else:
+				poly[len(poly)-1] = poly[len(poly)-1] + (inputstr[i])
+		else:
+			poly.append(inputstr[i])
+			ncounting = False
+	i += 1
+print(poly)
+
+#for i in range(-ansrange, ansrange):
+#	if evaluate(i) == 0:
+#		printf("(x")
+#		if(i < 0) print(" + " + -1 * x + ")")
+#		elif (i > 0) print(" - " + -1 * x + ")")
+#		else print(")")
